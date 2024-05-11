c4642edee503844b460a5143b789084cf8460a25
S Sabari Raj <sabarirajssr143@gmail.com>
2024-05-11 16:44:34 +0530

    Everything changed

--- frontend/src/chatForm.js
+++ frontend/src/chatForm.js
@@ -7,9 +7,13 @@ const Form = ({ handleSubmit, input, setInput }) => (
       className="input-field"
       value={input} 
       onChange={(e) => setInput(e.target.value)} 
-      placeholder="Type your message..." 
+      placeholder="Type your query..." 
     />
-    <button className="submit-btn" type="submit">Send</button>
+    <button className="submit-btn" type="submit">
+    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="30" viewBox="0 0 24 24" fill="black">
+        <path d="M2 21l21-9L2 3v7l15 2-15 2z"/>
+      </svg>
+    </button>
     <br></br>
     <br></br>
     <br></br>
